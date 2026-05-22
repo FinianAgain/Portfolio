f93a5724b84c04d4ac1b793d5a4a87715728eaef
MorriganAgain <onealf@oregonstate.edu>
2026-05-22 15:56:48 -0700

    update

--- sketch.js
+++ sketch.js
@@ -16,26 +16,26 @@ function setup() {
   let start_angles = [];
   for (let i=1; i <= num_links; i++) {
     links.push((width + 400 * i) / (i * 4));
-    start_angles.push(0);
+    start_angles.push(2*i-0.5);
   }
     test = new Linkage(
       createVector((width), height/2), 
       links, 
       start_angles); 
 
-  let ik_button = createA("Inverse Kinematics 1.1.3.pdf", "n-DOF Inverse Kinematics", '_blank');
+  let ik_button = createA("Inverse Kinematics 1.1.4.pdf", "n-DOF Inverse Kinematics", '_blank');
   ik_button.parent('p5-button');
   ik_button.class('button-link');
   ik_button.position(0, 0, 'relative');
   ik_button.mouseOver(() => set_point = createVector(mouseX, mouseY));
   
-  let ac_button = createA("", "Passive Gimbal for Aquaculure Monitoring - COMING SOON", '_blank');
+  let ac_button = createA("", "Camera Isolator for Aquaculure Monitoring Drone - COMING SOON", '_blank');
   ac_button.parent('p5-button');
   ac_button.class('button-link');
   ac_button.position(0, 0, 'relative');
   ac_button.mouseOver(() => set_point = createVector(mouseX, mouseY));
 
-  let FEM_button = createA("", "Finite Element Analysis - COMING SOON", '_blank');
+  let FEM_button = createA("", "3D Printed Cycloidal Drive - COMING SOON", '_blank');
   FEM_button.parent('p5-button');
   FEM_button.class('button-link');
   FEM_button.position(0, 0, 'relative');
